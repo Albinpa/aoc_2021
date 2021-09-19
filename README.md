377df240586ba6ecc70d601e40a33fb2019eafe1
albin.palsson <albin_palsson@hotmail.se>
2021-09-19 09:17:18 +0200

    Try out some c++20 ranges

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Repository for Advent of Code 2021
 
 `generate_skeleton.sh` is used create a subfolder with helpful tools to quickly solve a AOC problems with c++, 'script' style. 
 
-Requires make and g++.
+Requires make and g++-10 (for c++20 support).
--- shared/aoc_library.hpp
+++ shared/aoc_library.hpp
@@ -6,69 +6,77 @@
 #include <algorithm>
 #include <sstream>
 #include <unordered_map>
+#include <ranges>
+
+namespace ranges = std::ranges;
 
 namespace aoc
 {
-	inline std::vector<std::string> get_lines(const std::string &filename)
-	{
-		std::vector<std::string> res = {};
-		std::string line;
-		std::ifstream file(filename);
-		assert(file.is_open());
+    inline std::vector<std::string> get_lines(const std::string &filename)
+    {
+        std::vector<std::string> res = {};
+        std::string line;
+        std::ifstream file(filename);
+        assert(file.is_open());
 
-		while (getline(file, line))
-		{
-			res.push_back(line);
-		}
-		file.close();
+        while (getline(file, line))
+        {
+            res.push_back(line);
+        }
+        file.close();
 
-		return res;
-	}
+        return res;
+    }
 
-	inline std::vector<int> transform_int(const std::vector<std::string> &strings)
-	{
-		std::vector<int> ints;
-		std::transform(strings.begin(), strings.end(), back_inserter(ints),
-					   [](const std::string &s)
-					   { return std::stoi(s); });
-		return ints;
-	}
+    inline std::vector<int> transform_int(const std::vector<std::string> &strings)
+    {
+        const auto to_int = [](const std::string &s)
+        { return std::stoi(s); };
+        const auto ints = strings | ranges::views::transform(to_int);
+        return std::vector<int>(ints.begin(), ints.end());
+    }
 
-	template <typename T>
-	inline void assert_equal(const T actual, const T expected)
-	{
-		if (expected != actual)
-		{
-			std::cout << "assert_equal failed, actual: ";
-			std::cout << actual << " expected: " << expected << std::endl;
-			throw;
-		}
-	}
+    template <typename T>
+    inline void assert_equal(const T actual, const T expected)
+    {
+        if (expected != actual)
+        {
+            std::cout << "assert_equal failed, actual: ";
+            std::cout << actual << " expected: " << expected << std::endl;
+            throw;
+        }
+    }
 
-	template <typename T>
-	inline void print(const std::vector<T> &vector)
-	{
-		std::stringstream ss;
-		ss << "[";
-		for (const auto &i : vector) {
-				ss << i;
-				if (&i != &vector.back()) {
-					ss << ",";
-				}
-		}
-		std::cout << ss.str() << "]" << std::endl;
-	}
+    template <typename T>
+    inline void print(const std::vector<T> &v)
+    {
+        std::cout << "[";
+        ranges::for_each(v | ranges::views::take(v.size() - 1),
+                         [](const auto &i)
+                         { std::cout << i << ", "; });
+        std::cout << v.back() << "]" << std::endl;
+    }
 
-	template <typename T, typename K>
-	inline void print(const std::unordered_map<T,K> &map)
-	{
-		std::stringstream ss;
-		ss << "{";
-		for (const auto &i : map) {
-			ss << "{" << i.first << "," << i.second << "},";
-		}
-		ss.seekp(-1, ss.cur);
-		ss << "}";
-		std::cout << ss.str() << std::endl;
-	}
+    template <typename T>
+    inline void print(const std::vector<std::vector<T>> &v)
+    {
+        std::cout << "[" << std::endl;
+        ranges::for_each(v, [](const auto vi)
+                         {
+                             std::cout << " ";
+                             print(vi);
+                         });
+        std::cout << "]" << std::endl;
+    }
+
+    template <typename T, typename K>
+    inline void print(const std::unordered_map<T, K> &map)
+    {
+        const auto format = [](const auto &pair)
+        { std::cout << "{" << pair.first << ", " << pair.second << "}, "; };
+
+        std::cout << "{";
+        ranges::for_each(map, format);
+        std::cout << "}" << std::endl;
+    }
 }
